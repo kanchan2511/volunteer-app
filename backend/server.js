82a262ea300146805c5c866d758514cabec08c7f
kanchan2511 <kanchankumawat2509@gmail.com>
2026-02-08 23:07:29 +0530

    Add volunteer routes

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB connected"))
   .catch(err => console.log(err));
 
-app.use("/api/volunteers", require("./routes/volunteerRoutes"));
+app.use("/api/volunteers", require("./routes/VolunteerRoutes"));
 
 app.listen(process.env.PORT, () => {
     console.log(`Server running on port ${process.env.PORT}`)
